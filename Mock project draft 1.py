39fbbab5c684095feba0fb5c181612dbb0e9343e
Henry T-I <63914305+HenryThornton-Izzard@users.noreply.github.com>
2020-04-24 13:33:43 +0100

    Add files via upload
    Newer version

--- Mock project draft 1.py
+++ Mock project draft 1.py
@@ -9,25 +9,59 @@
 
 #Function to check the RLE length#
 def Enter_RLE(RLE_Length):
-    valid_length = "1234567890"
-    print("Enter RLE", RLE_Length)
+    for i in range(0, RLE_Length):
+        input("Please enter line 1")
+RLE_Length = 3
+aline = []
+for i in range (0, RLE_Length):
+    line = input("Enter RLE")
+    aline.append(line)
+    print(aline)
 
-    if len(RLE_Length) >= 2 and len(RLE_Length) <= 50:
-        print("The minimum is 2 lines, please enter more")
-    else:
-        return False
+for j in range (0, len(aline)):
+    cstr = aline[j]
+    for i in range(0, len(Cstr), 3):
+        num = int(Cstr[i]+Cstr[i+1])
+        print (num*Cstr[i+2], end="")
+    print()
+        
+    
+    
+
+    
+##a=[]
+##
+##b=input("Enter RLE")
+##
+##a.append[b]
+##
+##Cstr = "02A188"#
+##
+##for i in range (0, len(Cstr), 3):
+##    num = int(Cstr[i]+Cstr[i+1])
+##    print (num*Cstr[i+2], end="")
 
 #Function to Display ASCII art#
-def Display_ASCII(ASCII_art):
+##def Display_ASCII(ASCII_art):
 
 
-#Function to Convert to ASCII#  
-def Convert_ASCII(Convert_RLE):               
+#Function to convert ASCII art#
+#def Convert_ASCII(Convert_RLE):
+ #   F1 = open("LogoRLE.txt", "r")
+    #print (F1.read())
+
+  #  for i in F1.readlines():
+   #     for j in range(0, len(i)-1, 3):
+    #        num = int(i[j]+i[j+1])
+     #       print(num*i[j+2], end="")
+      #  print("\n")
+    #F1.close()#
 
 
 #--------------------------------------------------------------------------------------------#
-# The Main program                                                                           #
+# THE MAIN PROGRAM vvvvv                                                                     #
 #--------------------------------------------------------------------------------------------#
+
 #Introduction to the menu#
 
 print("********************************************")
@@ -57,7 +91,14 @@ while user_input != "4":
         print("How many lines of RLE compressed data would you like to enter?")
         print("It should be more than 2 lines long")
         print("If it's not, it won't work")
-        RLE_Length = input("RLE_Length: ")
+        RLE_Length = int(input("RLE_Length: "))
+        while RLE_Length <= 2:
+            RLE_Length = int(input("RLE_Length: "))
+            
+            
+
+        Enter_RLE(RLE_Length)
+        
 
     #Menu choice 2#
     elif user_input == "2":
@@ -71,32 +112,21 @@ while user_input != "4":
 
         #Calling the convert to ASCII function#
         print("Please enter the name of the text file containing the RLE data")
-        print(
+        print(Convert_RLE)
+
+    #Menu choice 4 / end of loop#
+    elif user_input == "4":
+        print("Goodbye for now :)")
+        exit
 
     else:
         print("Sorry! You can only choose 1, 2, 3 or 4")
 
-#End of while loop / Menu choice 4#
-print("Goodbye for now. :)")
-
-
- 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-        
 
 
 
